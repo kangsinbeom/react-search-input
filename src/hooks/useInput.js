11e7c70801cfa4f314268f08ad9504d75758eb82
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-01 22:15:58 +0900

    다시 시작해보자 애시당초 글러먹었음

--- src/hooks/useInput.js
+++
@@ -1,8 +0,0 @@
-// import { useEffect, useState } from "react";
-// import { apiHandler } from "../apis/apiHandler";
-
-const useInput = (input) => {
-  console.log(input);
-};
-
-export default useInput;
