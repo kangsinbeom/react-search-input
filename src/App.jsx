9ac2ed38a21d218dc96db17194294b4bef147e91
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-04 10:02:22 +0900

    axios 데이터 출력 성공

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import "./App.css";
 import axios from "axios";
 import instance from "./apis/api";
 function App() {
+  const [datas, setDatas] = useState([]);
+
   useEffect(() => {
     instance
       .get("/v2/search/web")
       .then((res) => {
         console.log("결과 : ", res);
+        setDatas(res.data.documents);
+        console.log(datas);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  return <></>;
+  return (
+    <>
+      {datas?.map((data, index) => (
+        <div key={index}>{data.contents}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
+
+// 데이터 출력까지 성공
